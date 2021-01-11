2b8282769b22173e53a0c91ddc490bd6797e241d
Gavin Gilmour <gavin.gilmour@gmail.com>
2021-01-11 20:10:06 +0000

    add more servers

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -72,7 +72,7 @@ end
 
 -- Use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
-local servers = { "tsserver" }
+local servers = { "bashls", "dockerls", "jsonls", "yamlls", "tsserver" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup { on_attach = on_attach }
 end
