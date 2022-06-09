d90a3439ddbe8b22fca5e517932052f55166defe
Gavin Gilmour <gavin.gilmour@gmail.com>
2022-06-09 20:11:13 +0100

    :fire: Remove comments

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,19 +2,15 @@ require("impatient").enable_profile()
 require("bootstrap")
 require("deps")
 
--- enable filetype.lua
 vim.g.do_filetype_lua = 1
-
--- map leader key to comma
 vim.g.mapleader = ","
 vim.g.maplocalleader = "<Space>"
 
--- order matters
 vim.cmd [[
-runtime! lua/modules/options.lua
-runtime! lua/modules/util.lua
-runtime! lua/modules/mappings.lua
-runtime! lua/modules/statusline.lua
+  runtime! lua/modules/options.lua
+  runtime! lua/modules/util.lua
+  runtime! lua/modules/mappings.lua
+  runtime! lua/modules/statusline.lua
 ]]
 
 vim.cmd [[ colorscheme yellow-moon ]]
