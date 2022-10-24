c18369bc3b2972e2885328bf85c2152e8249d2d2
Gavin Gilmour <gavin.gilmour@scotland.pnn.police.uk>
2022-10-24 11:21:58 +0100

    :alien: update_capabilities => default_capabilities

--- nvim/lua/modules/lsp/init.lua
+++ nvim/lua/modules/lsp/init.lua
@@ -4,9 +4,7 @@ local Util = require "modules.util"
 -- override handlers
 require "modules.lsp.handlers"
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(
-  vim.lsp.protocol.make_client_capabilities()
-)
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local servers = {
   tsserver = {
