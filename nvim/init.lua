29c3ec60c70b4e3c1cc0463485dc31e84c79ffb6
Gavin Gilmour <gavin.gilmour@gmail.com>
2023-12-13 20:05:47 +0000

    :wrench: change fillchars

--- nvim/init.lua
+++ nvim/init.lua
@@ -69,6 +69,7 @@ require('ufo').setup({
     end
 })
 
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 vim.o.foldcolumn = '1' -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
