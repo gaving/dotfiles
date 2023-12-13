9b27b1245641cb00e1b3fedf1a2abe429fa23b09
Gavin Gilmour <gavin.gilmour@gmail.com>
2023-12-13 20:05:31 +0000

    :wrench: use gpt4

--- nvim/lua/deps.lua
+++ nvim/lua/deps.lua
@@ -104,7 +104,25 @@ require("lazy").setup({
     "jackMort/ChatGPT.nvim",
     event = "VeryLazy",
     config = function()
-      require("chatgpt").setup()
+      require("chatgpt").setup({
+        openai_params = {
+          model = "gpt-4-1106-preview",
+          frequency_penalty = 0,
+          presence_penalty = 0,
+          max_tokens = 300,
+          temperature = 0,
+          top_p = 1,
+          n = 1,
+        },
+        openai_edit_params = {
+          model = "gpt-4-1106-preview",
+          frequency_penalty = 0,
+          presence_penalty = 0,
+          temperature = 0,
+          top_p = 1,
+          n = 1,
+        },
+      })
     end,
     dependencies = {
       "MunifTanjim/nui.nvim",
