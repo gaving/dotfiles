08ff5bf84644f39050b77eb8f81420da48165517
Gavin Gilmour <gavin.gilmour@gmail.com>
2023-12-17 14:56:13 +0000

    :lipstick: update theme
    Signed-off-by: Gavin Gilmour <gavin.gilmour@gmail.com>

--- nvchad/chadrc.lua
+++ nvchad/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "bearded-arc",
-  theme_toggle = { "tokyonight", "bearded-arc" },
+  theme = "catppuccin",
+  theme_toggle = { "tokyonight", "catppuccin" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
