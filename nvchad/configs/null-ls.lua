a7f59301a94a2921d5ebcdc0e6dfafcae71f88f1
Gavin Gilmour <gavin.gilmour@gmail.com>
2024-01-03 14:48:38 +0000

    :wrench: Add biome config

--- nvchad/configs/null-ls.lua
+++ nvchad/configs/null-ls.lua
@@ -5,7 +5,6 @@ local b = null_ls.builtins
 local sources = {
 
   -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
   b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
 
   -- Lua
--- nvchad/plugins.lua
+++ nvchad/plugins.lua
@@ -91,6 +91,33 @@ local plugins = {
     "williamboman/mason.nvim",
     opts = overrides.mason
   },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = overrides.mason
+  },
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        -- Customize or remove this keymap to your liking
+        "<leader>F",
+        function()
+          require("conform").format({ async = true, lsp_fallback = true })
+        end,
+        mode = "",
+        desc = "Format buffer",
+      },
+    },
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { { "biomejs" } },
+      },
+      format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    }
+  }
 }
 
 return plugins
